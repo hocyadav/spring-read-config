03b0dc66e62d3638ab775b497d18fd7cde461cdc
Hariom Yadav <hoc.yadav@gmail.com>
2021-02-07 10:46:46 +0530

    updated

--- src/main/java/com/example/springreadconfig/YmlConfig.java
+++ src/main/java/com/example/springreadconfig/YmlConfig.java
@@ -3,7 +3,6 @@ package com.example.springreadconfig;
 import java.util.List;
 import java.util.Map;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.stereotype.Component;
 import lombok.Getter;
 import lombok.Setter;
@@ -13,26 +12,20 @@ import lombok.ToString;
  * @author HariomYadav
  * @since 24/01/21
  */
-@Component
+@Component //or @Configuration
 @ConfigurationProperties (prefix = "ymlkey1")//1. root as key : parent field name
 @Getter
 @Setter
-@ToString
 public class YmlConfig {
     Map<String, String> application;//child key name 1
-
     Map<String, List<String>> config;//child key name2
-
     Map<String, ClassA> users;//child key name3
-
     Map<String, Details> all_users_config;//child key name4.. so total 4 entries in above "ymlkey1"
 
     @Getter
     @Setter
-    @ToString
     public static class ClassA {//static class : imp
         String username;
-
         String password;
     }
 
@@ -41,11 +34,8 @@ public class YmlConfig {
     @ToString
     private static class Details {
         List<LogType> logType;
-
         Integer logFrequency;
-
         Integer logDurationInSec;
-
         Integer logWaitTimeInSec;
     }
 }
